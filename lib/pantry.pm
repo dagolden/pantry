93adeefd4b4ce39eab53a2c2953e111ae969b6b5
David Golden <dagolden@cpan.org>
2011-12-06 15:26:56 -0500

    initial import of pantry

---
+++ lib/pantry.pm
@@ -0,0 +1,35 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package pantry;
+# ABSTRACT: No abstract given for pantry
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use pantry;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+Maybe other modules do related things.
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
