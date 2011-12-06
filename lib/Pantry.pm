07b79fa8d05a76ae0ff82d6d6569e7cd1cd12876
David Golden <dagolden@cpan.org>
2011-12-06 15:54:16 -0500

    initial app and documentation skeleton

---
+++ lib/Pantry.pm
@@ -0,0 +1,20 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package Pantry;
+# ABSTRACT: Manage resources for chef-solo
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+# This file is a namespace placeholder and gives a default place to find
+# documentation for the 'pantry' program.
+
+# Pod for this file is generated from the pod/ directory in the source
+# repository using the 'AppendExternalData' dzil plugin
+
+1;
+
+# vim: ts=2 sts=2 sw=2 et:
