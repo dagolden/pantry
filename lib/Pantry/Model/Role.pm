58ce31944044be0e9be0877d63ed3e5596a8e0c0
David Golden <dagolden@cpan.org>
2012-04-27 14:36:21 -0400

    added experimental Role class

---
+++ lib/Pantry/Model/Role.pm
@@ -0,0 +1,62 @@
+use v5.14;
+use warnings;
+
+package Pantry::Model::Role;
+# ABSTRACT: Pantry data model for roles
+# VERSION
+
+use Moose 2;
+use List::AllUtils qw/uniq first/;
+use namespace::autoclean;
+
+# new_from_file, save_as
+with 'Pantry::Role::Serializable' => {
+  engine => 'JSON',
+  engine_opts => { utf8 => 1, pretty => 1 }
+};
+
+# in_run_list, append_to_runliset
+with 'Pantry::Role::Runlist';
+
+#--------------------------------------------------------------------------#
+# static keys/values required by Chef
+#--------------------------------------------------------------------------#
+
+has chef_type => (
+  is => 'bare',
+  isa => 'Str',
+  default => 'role',
+  init_arg => undef,
+);
+
+has json_class => (
+  is => 'bare',
+  isa => 'Str',
+  default => 'Chef::Role',
+  init_arg => undef,
+);
+
+#--------------------------------------------------------------------------#
+# Chef role attributes
+#--------------------------------------------------------------------------#
+
+has name => (
+  is => 'ro',
+  isa => 'Str',
+  required => 1,
+);
+
+has description => (
+  is => 'ro',
+  isa => 'Str',
+  lazy => 1,
+);
+
+sub _build_description {
+  my $self = shift;
+  return "The " . $self->name . " role";
+}
+
+
+1;
+
