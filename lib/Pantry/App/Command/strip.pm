42c1b0c084abb46ab6578fcbb6a51956066200ad
David Golden <dagolden@cpan.org>
2012-05-02 11:06:44 -0400

    add pantry strip command for recipes

---
+++ lib/Pantry/App/Command/strip.pm
@@ -0,0 +1,78 @@
+use v5.14;
+use warnings;
+
+package Pantry::App::Command::strip;
+# ABSTRACT: Implements pantry strip subcommand
+# VERSION
+
+use Pantry::App -command;
+use autodie;
+
+use namespace::clean;
+
+sub abstract {
+  return 'strip recipes or attributes from a node'
+}
+
+sub options {
+  return;
+}
+
+sub validate {
+  my ($self, $opts, $args) = @_;
+  my ($type, $name) = @$args;
+
+  # validate type
+  if ( ! length $type ) {
+    $self->usage_error( "This command requires a target type and name" );
+  }
+  elsif ( $type ne 'node' ) {
+    $self->usage_error( "Invalid type '$type'" );
+  }
+
+  # validate name
+  if ( ! length $name ) {
+    $self->usage_error( "This command requires the name for the thing to modify" );
+  }
+
+  return;
+}
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  my ($type, $name) = splice(@$args, 0, 2);
+
+  if ( $type eq 'node' ) {
+    my $node = $self->pantry->node( $name )
+      or $self->usage_error( "Node '$name' does not exist" );
+
+    if ($opt->{recipe}) {
+      $node->remove_from_runlist(map { "recipe[$_]" } @{$opt->{recipe}});
+    }
+
+# XXX copy/paste from apply -- save for later use for strip
+#    if ($opt->{default}) {
+#      for my $attr ( @{ $opt->{default} } ) {
+#        my ($key, $value) = split /=/, $attr, 2; # split on first '='
+#        if ( $value =~ /(?<!\\),/ ) {
+#          # split on unescaped commas, then unescape escaped commas
+#          $value = [ map { s/\\,/,/gr } split /(?<!\\),/, $value ];
+#        }
+#        $node->set_attribute($key, $value);
+#      }
+#    }
+
+    $node->save;
+  }
+
+  return;
+}
+
+1;
+
+=for Pod::Coverage options validate
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
