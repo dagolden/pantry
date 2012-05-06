de6791d7c06df7f9b5e33c9e06c01e765e636fac
David Golden <dagolden@cpan.org>
2012-05-05 22:16:16 -0400

    add role deletion

--- lib/Pantry/App/Command/delete.pm
+++ lib/Pantry/App/Command/delete.pm
@@ -19,7 +19,7 @@ sub command_type {
 }
 
 sub valid_types {
-  return qw/node/
+  return qw/node role/
 }
 
 sub options {
@@ -30,21 +30,31 @@ sub options {
 
 sub _delete_node {
   my ($self, $opt, $name) = @_;
+  $self->_delete_obj($opt, "node", $name);
+}
 
-  my $node = $self->pantry->node( $name );
-  if ( ! -e $node->path ) {
-    die( "Node '$name' doesn't exist\n" );
+sub _delete_role {
+  my ($self, $opt, $name) = @_;
+  $self->_delete_obj($opt, "role", $name);
+}
+
+sub _delete_obj {
+  my ($self, $opt, $type, $name) = @_;
+
+  my $obj = $self->pantry->$type( $name );
+  if ( ! -e $obj->path ) {
+    die( "obj '$name' doesn't exist\n" );
   }
 
   unless ( $opt->{force} ) {
-    my $confirm = IO::Prompt::Tiny::prompt("Delete node '$name'?", "no");
+    my $confirm = IO::Prompt::Tiny::prompt("Delete $type '$name'?", "no");
     unless ($confirm =~ /^y(?:es)?$/i) {
       print "$name will not be deleted\n";
       exit 0;
     }
   }
 
-  unlink $node->path;
+  unlink $obj->path;
 
   return;
 }
